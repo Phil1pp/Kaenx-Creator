0c4d0cda0dff4dddac1fa21a7b1a65877b357130
Phil1pp <15369870+Phil1pp@users.noreply.github.com>
2023-02-23 22:02:36 +0100

    Show name and function text in ComObjectList

---
+++ Kaenx.Creator/Converter/ComObjectToString.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using System.Windows.Data;
+using Kaenx.Creator.Models;
+
+namespace Kaenx.Creator.Converter
+{
+    public class ComObjectToString : IValueConverter
+    {
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return ((ComObject)value).Name + " - " + ((ComObject)value).FunctionText[0].Text;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new Exception("not supported");
+        }
+    }
+}
